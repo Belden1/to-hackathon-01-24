356cff020e01c2c1819a2384698de21407526626
Kristopher Hellsten <carvajalhellstenmail@gmail.com>
2024-02-15 23:33:00 +0100

    Added missing 'active' functionality

--- script.js
+++ script.js
@@ -12,20 +12,38 @@ const submitButton = document.getElementById('submit');
 
 let rating = 0;
 
+function removeActiveClass() {
+  ratingCircleOne.classList.remove('active');
+  ratingCircleTwo.classList.remove('active');
+  ratingCircleThree.classList.remove('active');
+  ratingCircleFour.classList.remove('active');
+  ratingCircleFive.classList.remove('active');
+}
+
 ratingCircleOne.addEventListener('click', function () {
   rating = 1;
+  removeActiveClass();
+  ratingCircleOne.classList.add('active');
 });
 ratingCircleTwo.addEventListener('click', function () {
   rating = 2;
+  removeActiveClass();
+  ratingCircleTwo.classList.add('active');
 });
 ratingCircleThree.addEventListener('click', function () {
   rating = 3;
+  removeActiveClass();
+  ratingCircleThree.classList.add('active');
 });
 ratingCircleFour.addEventListener('click', function () {
   rating = 4;
+  removeActiveClass();
+  ratingCircleFour.classList.add('active');
 });
 ratingCircleFive.addEventListener('click', function () {
   rating = 5;
+  removeActiveClass();
+  ratingCircleFive.classList.add('active');
 });
 
 submitButton.addEventListener('click', function () {
