8eea414f102ec9cf62bae480e4b36b94b5ed79b6
Kristopher Hellsten <carvajalhellstenmail@gmail.com>
2024-02-15 23:25:53 +0100

    Adding prettier

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
-const thankyouDiv = document.getElementById("thankyouid");
-const howdidwedoId = document.getElementById("howdidwedoId");
-const scoreId = document.getElementById("score");
+const thankyouDiv = document.getElementById('thankyouid');
+const howdidwedoId = document.getElementById('howdidwedoId');
+const scoreId = document.getElementById('score');
 
-thankyouDiv.style.display = "none";
-const ratingCircleOne = document.getElementById("one");
-const ratingCircleTwo = document.getElementById("two");
-const ratingCircleThree = document.getElementById("three");
-const ratingCircleFour = document.getElementById("four");
-const ratingCircleFive = document.getElementById("five");
-const submitButton = document.getElementById("submit");
+thankyouDiv.style.display = 'none';
+const ratingCircleOne = document.getElementById('one');
+const ratingCircleTwo = document.getElementById('two');
+const ratingCircleThree = document.getElementById('three');
+const ratingCircleFour = document.getElementById('four');
+const ratingCircleFive = document.getElementById('five');
+const submitButton = document.getElementById('submit');
 
 let rating = 0;
 
-ratingCircleOne.addEventListener("click", function () {
+ratingCircleOne.addEventListener('click', function () {
   rating = 1;
 });
-ratingCircleTwo.addEventListener("click", function () {
+ratingCircleTwo.addEventListener('click', function () {
   rating = 2;
 });
-ratingCircleThree.addEventListener("click", function () {
+ratingCircleThree.addEventListener('click', function () {
   rating = 3;
 });
-ratingCircleFour.addEventListener("click", function () {
+ratingCircleFour.addEventListener('click', function () {
   rating = 4;
 });
-ratingCircleFive.addEventListener("click", function () {
+ratingCircleFive.addEventListener('click', function () {
   rating = 5;
 });
 
-submitButton.addEventListener("click", function () {
+submitButton.addEventListener('click', function () {
   if (rating > 0) {
-    howdidwedoId.style.display = "none";
+    howdidwedoId.style.display = 'none';
     scoreId.textContent = rating;
-    thankyouDiv.style.display = "flex";
+    thankyouDiv.style.display = 'flex';
   } else {
     return null;
   }
